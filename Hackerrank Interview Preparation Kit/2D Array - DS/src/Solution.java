53cf8d6202f34d78571e4f648cb027d4ba04d513
DNDimitrov <d_sunfire@abv.bg>
2019-09-08 13:47:28 +0300

    fixing bug with max of hourglass

--- Hackerrank Interview Preparation Kit/2D Array - DS/src/Solution.java
+++ Hackerrank Interview Preparation Kit/2D Array - DS/src/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
     // Complete the hourglassSum function below.
     static int hourglassSum(int[][] arr) {
         int sum = 0;
-        int max = 0;
+        int max = Integer.MIN_VALUE;
         for(int i = 0;i<arr.length -2 ;i++) {
             for(int j = 0;j<arr.length -2; j++) {
                 sum = sum(arr,i,j);
